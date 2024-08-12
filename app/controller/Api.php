48150c01fe624e14481969b34b69acff5ea3cfc9
JulJulse <152330795+JulJulse@users.noreply.github.com>
2024-08-12 10:48:55 +0300

    Add files via upload

---
+++ app/controller/Api.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use App\Model\Message;
+use Base\AbstractController;
+
+class Api extends AbstractController
+{
+    public function index()
+    {
+       echo 123;
+}
